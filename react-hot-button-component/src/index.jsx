a39a1d042f87d44a963e8165c6af2315346f398c
knguyen2453 <yeaitskevinn@yahoo.com>
2020-12-01 16:49:26 -0800

    Completed React-Hot-Button-Component

---
+++ react-hot-button-component/src/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import CustomHotButton from './hot-button';
+
+ReactDOM.render(
+  <CustomHotButton />,
+  document.getElementById('root')
+);
